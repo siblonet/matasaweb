dc36114ac8dca69d678e9fcc4a5dd7b327609a57
John Kokar <princedibloni4@gmail.com>
2024-03-16 18:20:22 +0000

    fgroo

--- functionServices/wedgeimages.js
+++ functionServices/wedgeimages.js
@@ -2,7 +2,7 @@ async function setLogin() {
     const items = await GetSettings();
     //console.log(items);
     if (items && items.length > 0) {
-        $(".nobleim").attr("src", `${items.find(item => item.which === "logoextern") ? items.find(item => item.which === "logoextern").image : "assets/img/logo.png"}`);
+        $(".nobleim").attr("src", `${items.find(item => item.which === "logoextern") ? items.find(item => item.which === "logoextern").image : "assets/img/matasa.png"}`);
         $(".page-linear").css("background-image", `url(${items.find(item => item.which === "loginimg") ? items.find(item => item.which === "loginimg").image : "assets/img/par1.png"})`);
         $(".bg-image1").css("background-image", `url(${items.find(item => item.which === "backglise") ? items.find(item => item.which === "backglise").image : "assets/img/par1.png"})`);
     } else {
